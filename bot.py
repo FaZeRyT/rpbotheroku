ad30dda4e665a8bf3d26a5694165653919a1b07d
FaZeRyT <61416952+FaZeRyT@users.noreply.github.com>
2020-08-20 17:45:03 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,18 +10,14 @@ bot = commands.Bot(command_prefix='!')
 
 @bot.command(pass_context=True)
 async def msg(ctx, *, arg):
-    await ctx.send(arg)  
-    print('Сообщение с текстом "' + str(arg) + '" отправлено!')
+    await ctx.send(arg) 
 
 @bot.command(pass_context=True)   
 async def h(ctx):
     await ctx.send('*Типо список комманд*')
-    print('Выведен список комманд.')
 
 @bot.command(pass_context=True)   
 async def ban(ctx, user: discord.User):
     await ctx.send('Пользователь ' + user.name + ' забанен пользователем ' + format(ctx.message.author))
-    print(user.name + ' забанен!')
 
-token = os.environ.get('BOT_TOKEN')
-bot.run(str(token))
+bot.run(str(os.environ.get('BOT_TOKEN')))
